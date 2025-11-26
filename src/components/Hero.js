91ac0309ba9a22fb9bad3e5ba8a5ad135c8d279f
tandelkirtan <kirtandel111@gmail.com>
2025-11-27 01:38:10 +0530

    header and home linked

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import { Chat } from "./Chat";
 export const Hero = ({ isMobile }) => {
   return (
     <Box sx={{ width: isMobile ? "100%" : "80%", height: "100%" }}>
-      <Box width="100%" height="8%">
-        <Typography component="header" fontWeight={800} p={1} pl={isMobile ? 6 : 1} fontSize={30} color="#9860f9ff">BOT AI</Typography>
+      <Box component="header" width="100%" height="8%">
+        <Typography component="h1" fontWeight={800} p={1} pl={isMobile ? 6 : 1} fontSize={30} color="#9860f9ff">Bot AI</Typography>
       </Box>
       <Box width="100%" height="92%">
         <Chat />
