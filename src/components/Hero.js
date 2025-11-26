bc61b44ea070eaeb7fec6d337cfcef5191edbf42
tandelkirtan <kirtandel111@gmail.com>
2025-11-26 22:03:04 +0530

    headder added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ export const Hero = ({ isMobile }) => {
   return (
     <Box sx={{ width: isMobile ? "100%" : "80%", height: "100%" }}>
       <Box width="100%" height="8%">
-        <Typography fontWeight={800} p={1} pl={isMobile ? 6 : 1} fontSize={30} color="#9860f9ff">BOT AI</Typography>
+        <Typography component="header" fontWeight={800} p={1} pl={isMobile ? 6 : 1} fontSize={30} color="#9860f9ff">BOT AI</Typography>
       </Box>
       <Box width="100%" height="92%">
         <Chat />
